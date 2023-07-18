a9fd02aa8cb7a66dc84bf55a91b8c72c4a419cb8
CristianCM17 <cristiandavidcardoso@gmail.com>
2023-07-18 01:23:13 -0600

    el if con el selector

--- js/app.js
+++ js/app.js
@@ -1,49 +1,49 @@
-import Ingresos from '/js/ingresos.js';
-import Agregar from '/js/agregar.js';
-import Egresos from '/js/egresos.js';
+import Ingresos from "/js/ingresos.js";
+import Agregar from "/js/agregar.js";
+import Egresos from "/js/egresos.js";
 
-document.addEventListener('DOMContentLoaded',function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const agregar = new Agregar();
+  const ingresos = new Ingresos();
+  const egresos = new Egresos();
 
-        const agregar = new Agregar();
-        const ingresos= new Ingresos();
-        const egresos = new Egresos();
-        
-        agregar.setIngresos(ingresos);
-        agregar.setEgresos(egresos);
-        
-        const btnAgregar=document.getElementById('btnAgregar');
-        const AgregarDescipcion= document.getElementById('AgregarDescipcion'); 
-        const Agregarvalor= document.getElementById('Agregarvalor');
-        const IngresosPrincipal= document.getElementById('ingresoPrincipal');
+  agregar.setIngresos(ingresos);
+  agregar.setEgresos(egresos);
 
+  const btnAgregar = document.getElementById("btnAgregar");
+  const AgregarDescipcion = document.getElementById("AgregarDescipcion");
+  const Agregarvalor = document.getElementById("Agregarvalor");
+  const IngresosPrincipal = document.getElementById("ingresoPrincipal");
+  const selectElement = document.getElementById("tipo");
 
-        
+  
+
+  btnAgregar.onclick = () => {
+
+        const cambiarValor = parseFloat(Agregarvalor.value);
+
+   
+      // Obtener el valor seleccionado del select
+      const selectedValue = selectElement.value;
+
       
-       btnAgregar.onclick= () => {
-       // const cambiarValor=parseFloat(Agregarvalor.value);
-        //const ingreso= ingresos.agregarIngreso(AgregarDescipcion.value,cambiarValor);
-        //agregar.crearFila(ingreso);
-       // IngresosPrincipal.innerHTML=ingresos.sumar();
+      if (selectedValue === "ingreso") {
+        const ingreso = ingresos.agregarIngreso(AgregarDescipcion.value,cambiarValor);
+        agregar.crearFilaIngreso(ingreso);
+              // IngresosPrincipal.innerHTML=ingresos.sumar();
+      } else if (selectedValue === "egreso") {
+        const cambiarValor = parseFloat(Agregarvalor.value);
+        const egreso = egresos.agregarEgreso(AgregarDescipcion.value, cambiarValor);
+        agregar.crearFilaEgreso(egreso);
+      } else {
+        console.log("Valor desconocido");
+      }
+    
 
-       const cambiarValor=parseFloat(Agregarvalor.value);
-       const egreso= egresos.agregarEgreso(AgregarDescipcion.value,cambiarValor);
-       agregar.crearFilaEgreso(egreso);
-
-       }
-
-       
-
-
-
+   
 
 
-                
-        
 
-     
 
-       
-
-       
-      
+  };
 });
